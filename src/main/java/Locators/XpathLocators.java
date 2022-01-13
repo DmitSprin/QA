ab94e58094016e67d72afeb7fe969d5ada41b64c
Dima Spirin <spirin.dmitriy@tech-stack.io>
2022-01-13 16:10:31 +0200

    refactor xpath locators

--- src/main/java/Locators/XpathLocators.java
+++ src/main/java/Locators/XpathLocators.java
@@ -7,27 +7,27 @@ import java.util.List;
 
 public class XpathLocators {
     //using child
-    @FindBy(xpath = "//button //div[@class='pr']//child::*")
-    WebElement logIn;
+    @FindBy(xpath = ".//button //div[@class='pr']//child::*")
+    private WebElement logIn;
 
     //using text() and boolean 'or'
-    @FindBy(xpath = "//*[@class='df aic jcsb'] //*[text()='Смартфони' or text()='Смартфоны']")
-    WebElement smartphoneFromCatalog;
+    @FindBy(xpath = ".//*[@class='df aic jcsb'] //*[text()='Смартфони' or text()='Смартфоны']")
+    private WebElement smartphoneFromCatalog;
 
     //using function 'starts-with'
-    @FindBy(xpath = "//span[starts-with(text(),'044')]")
-    WebElement contactNumber;
+    @FindBy(xpath = ".//span[starts-with(text(),'044')]")
+    private WebElement contactNumber;
 
     //using function 'contains'
-    @FindBy(xpath = "//a[contains(@class,'dn aic jcc favorites')]")
-    WebElement wishList;
+    @FindBy(xpath = ".//a[contains(@class,'dn aic jcc favorites')]")
+    private WebElement wishList;
 
     //using following-sibling
-    @FindBy(xpath = "//ul//following-sibling::li")
-    List<WebElement> headerList;
+    @FindBy(xpath = ".//ul//following-sibling::li")
+    private List<WebElement> headerList;
 
     //using boolean '>' operator
-    @FindBy(xpath = "//span[@class='medium price-0-2-157' and text()>'800']")
-    List<WebElement> productWithMoreThen800Price;
+    @FindBy(xpath = ".//span[@class='medium price-0-2-157' and text()>'800']")
+    private List<WebElement> productWithMoreThen800Price;
 
 }
